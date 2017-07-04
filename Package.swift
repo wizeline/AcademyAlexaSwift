8c87de917cae2a726498c8ec05429a08c2bb07cc
Osuka42g <osuka42@gmail.com>
2017-07-04 13:10:01 +0700

    Update dependences

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftEcho",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1, minor: 7)
     ]
 )
